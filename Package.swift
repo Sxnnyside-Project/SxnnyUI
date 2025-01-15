2de3aa631928abad2f4418f89b6d58e3266b7304
CyMEZ <houjouzetaalpha@gmail.com>
2025-01-15 15:15:15 -0600

    Added FocusText item

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SxnnyUI",
+    platforms: [.iOS(.v15), .macOS(.v10_15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -15,7 +16,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SxnnyUI"),
+            name: "SxnnyUI",
+            dependencies: []),
         .testTarget(
             name: "SxnnyUITests",
             dependencies: ["SxnnyUI"]
